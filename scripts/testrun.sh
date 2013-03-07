6341dda861b10efa4c6d1ffb9d6e02efdeaf410f
Mark Chapman <mark.chapman@gmail.com>
2013-03-07 23:48:16 +0000

    Run server for e2e test

--- scripts/testrun.sh
+++ scripts/testrun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#./scripts/web-server.js > /dev/null &
-#sleep 1 # give server time to start
+node server/server.js > /dev/null &
+sleep 1 # give server time to start
 testacular start config/testacular.conf.js --no-auto-watch --single-run --reporters=dots --browsers=PhantomJS
 testacular start config/testacular-e2e.conf.js --reporters=dots --browsers=PhantomJS
