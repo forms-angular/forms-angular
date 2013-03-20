142efae7ca8969eb8723148b66965b711ae4cf2e
Mark Chapman <mark.chapman@gmail.com>
2013-03-20 01:24:23 +0000

    Add some explanation

--- app/js/app.js
+++ app/js/app.js
@@ -9,16 +9,16 @@ var myApp = angular.module('myApp', services).
         $routeProvider.
             when('/index', {templateUrl: 'partials/howto.html'} ).
             when('/404', {templateUrl: 'partials/404.html'} ).
-            when('/z_custom_form/new', {templateUrl: 'partials/custom-new.html', controller: BaseCtrl}).   // example view override
-            when('/z_custom_form/:id/edit', {templateUrl: 'partials/custom-edit.html', controller: BaseCtrl}).   // example view override
-            when('/z_custom_form/:form/new', {templateUrl: 'partials/custom-new.html', controller: BaseCtrl}).   // example view override
-            when('/z_custom_form/:form/:id/edit', {templateUrl: 'partials/custom-edit.html', controller: BaseCtrl}).   // example view override
+            when('/z_custom_form/new', {templateUrl: 'partials/custom-new.html', controller: BaseCtrl}).            // example view override
+            when('/z_custom_form/:id/edit', {templateUrl: 'partials/custom-edit.html', controller: BaseCtrl}).      // example view override
+            when('/z_custom_form/:form/new', {templateUrl: 'partials/custom-new.html', controller: BaseCtrl}).      // example view override with specified form content
+            when('/z_custom_form/:form/:id/edit', {templateUrl: 'partials/custom-edit.html', controller: BaseCtrl}).// example view override with specified form content
             when('/:model/:id/edit', {templateUrl: 'partials/base-edit.html', controller: BaseCtrl}).
             when('/:model/new', {templateUrl: 'partials/base-edit.html', controller: BaseCtrl}).
             when('/:model', {templateUrl: 'partials/base-list.html', controller: BaseCtrl}).
-            when('/:model/:form/:id/edit', {templateUrl: 'partials/base-edit.html', controller: BaseCtrl}).
-            when('/:model/:form/new', {templateUrl: 'partials/base-edit.html', controller: BaseCtrl}).
-            when('/:model/:form', {templateUrl: 'partials/base-list.html', controller: BaseCtrl}).
+            when('/:model/:form/:id/edit', {templateUrl: 'partials/base-edit.html', controller: BaseCtrl}).  // non default form (different fields etc)
+            when('/:model/:form/new', {templateUrl: 'partials/base-edit.html', controller: BaseCtrl}).       // non default form (different fields etc)
+            when('/:model/:form', {templateUrl: 'partials/base-list.html', controller: BaseCtrl}).           // list page with links to non default form
             otherwise({redirectTo: '/index'});
     }]);
 
