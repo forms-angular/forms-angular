86991643ef13deaada42620dd49b60952e8b9338
Mark Chapman <mark.chapman@gmail.com>
2013-03-20 01:26:15 +0000

    Add a simplified ng-repeat directive to fix nested schemas

--- app/js/directives/form.js
+++ app/js/directives/form.js
@@ -1,25 +1,25 @@
 angular.module('formsAngular.form', [])
 
-    .directive('formInput', function($compile){
+    .directive('formInput', function ($compile) {
         return {
             restrict: 'E',
             replace: true,
             priority: 1,
-            compile: function(){
-                return function (scope, element, attrs){
-                    scope.$watch(attrs.formInput, function(){
+            compile: function () {
+                return function (scope, element, attrs) {
+                    scope.$watch(attrs.formInput, function () {
 
-                        var generateInput = function(fieldInfo, modelString, isRequired, idString) {
+                        var generateInput = function (fieldInfo, modelString, isRequired, idString) {
                             var focusStr = '';
                             if (!modelString) {
                                 if (fieldInfo.name.indexOf('.') != -1 && element[0].outerHTML.indexOf('schema="true"') != -1) {
                                     // Schema handling - need to massage the ngModel and the id
                                     var compoundName = fieldInfo.name,
                                         lastPartStart = compoundName.lastIndexOf('.');
-                                    modelString = 'record.'+compoundName.slice(0,lastPartStart)+'.'+scope.$parent.$index+'.'+compoundName.slice(lastPartStart+1);
-                                    idString = modelString.replace(/\./g,'-')
+                                    modelString = 'record.' + compoundName.slice(0, lastPartStart) + '.' + scope.$parent.$index + '.' + compoundName.slice(lastPartStart + 1);
+                                    idString = modelString.replace(/\./g, '-')
                                 } else {
-                                    modelString = 'record.'+fieldInfo.name;
+                                    modelString = 'record.' + fieldInfo.name;
                                     if (scope.$index === 0) {
                                         focusStr = "autofocus ";
                                     }
@@ -29,13 +29,13 @@ angular.module('formsAngular.form', [])
                                 , requiredStr = (isRequired || fieldInfo.required) ? ' required' : '';
 
                             if (fieldInfo.type == 'select') {
-                                value = '<select ' + focusStr + 'ng-model="' + modelString + '" id="' + idString + '">';
+                                value = '<select ' + focusStr + 'ng-model="' + modelString + '" id="' + idString + '" name="' + idString + '">';
                                 value += '<option ng-repeat="option in ' + fieldInfo.options + '">{{option}}</option>';
                                 value += '</select>';
                             } else if (fieldInfo.type == 'textarea') {
-                                value = '<textarea ' + focusStr + (fieldInfo.rows ? 'rows = "'+ fieldInfo.rows +'" ':'') + 'ng-model="' + modelString + '"' + (idString ? ' id="'+ idString + '"': '') + requiredStr + (fieldInfo.add ? fieldInfo.add : '') + ' />';
+                                value = '<textarea ' + focusStr + (fieldInfo.rows ? 'rows = "' + fieldInfo.rows + '" ' : '') + 'ng-model="' + modelString + '"' + (idString ? ' id="' + idString + '" name="' + idString + '"' : '') + requiredStr + (fieldInfo.add ? fieldInfo.add : '') + ' />';
                             } else {
-                                value = '<input ' + focusStr + 'type="' + info.type + '" ng-model="' + modelString + '"' + (idString ? ' id="'+ idString + '"': '') + requiredStr + (fieldInfo.add ? fieldInfo.add : '') + '/>';
+                                value = '<input ' + focusStr + 'type="' + info.type + '" ng-model="' + modelString + '"' + (idString ? ' id="' + idString + '" name="' + idString + '"' : '') + requiredStr + (fieldInfo.add ? fieldInfo.add : '') + '/>';
                             }
                             if (fieldInfo.helpInline) {
                                 value += '<span class="help-inline">' + fieldInfo.helpInline + '</span>';
@@ -46,35 +46,35 @@ angular.module('formsAngular.form', [])
                             return value;
                         };
 
-                        var generateLabel = function(fieldInfo, addButton) {
+                        var generateLabel = function (fieldInfo, addButton) {
                             var labelHTML = '';
                             if (fieldInfo.label !== '' || addButton) {
-                                labelHTML = '<label class="control-label" for="'+fieldInfo.id+'">'+fieldInfo.label + (addButton || '')+'</label>';
+                                labelHTML = '<label class="control-label" for="' + fieldInfo.id + '">' + fieldInfo.label + (addButton || '') + '</label>';
                             }
                             return labelHTML;
                         };
 
-                        var handleField = function(info) {
-                            var template = '<div class="control-group" id="cg_'+info.id+'">';
+                        var handleField = function (info) {
+                            var template = '<div class="control-group" id="cg_' + info.id + '">';
                             if (info.schema) {
                                 //schemas (which means they are arrays in Mongoose)
-                                template += '<div class="schema-head well">'+info.label+'</div>'+
-                                    '<div class="sub-doc well" id="' + info.id + 'List" ng-repeat="subDoc in record.' + info.name + '">' +
-                                        '<div class="row-fluid">'+
-                                            '<div class="pull-left">'+
-                                                '<form-input ng-repeat="field in formSchema['+ scope.$index +'].schema" info="{{field}}" schema="true"></form-input>'+
-                                            '</div>' +
-                                            '<div class="pull-left sub-doc-btns">'+
-                                                '<button id="remove_'+info.id + '_btn" class="btn btn-mini form-btn" ng-click="remove(this,$index)">'+
-                                                    '<i class="icon-minus"></i> Remove'+
-                                                '</button>'+
-                                            '</div> '+
-                                        '</div>' +
-                                    '</div>'+
+                                template += '<div class="schema-head well">' + info.label + '</div>' +
+                                    '<div class="sub-doc well" id="' + info.id + 'List" ng-subdoc-repeat="subDoc in record.' + info.name + '">' +
+                                    '<div class="row-fluid">' +
+                                    '<div class="pull-left">' +
+                                    '<form-input ng-repeat="field in formSchema[' + scope.$index + '].schema" info="{{field}}" schema="true"></form-input>' +
+                                    '</div>' +
+                                    '<div class="pull-left sub-doc-btns">' +
+                                    '<button id="remove_' + info.id + '_btn" class="btn btn-mini form-btn" ng-click="remove(this,$index)">' +
+                                    '<i class="icon-minus"></i> Remove' +
+                                    '</button>' +
+                                    '</div> ' +
+                                    '</div>' +
+                                    '</div>' +
                                     '<div class = "schema-foot well">' +
-                                        '<button id="add_'+info.id + '_btn" class="btn btn-mini form-btn" ng-click="add(this)">'+
-                                            '<i class="icon-plus"></i> Add'+
-                                        '</button>'+
+                                    '<button id="add_' + info.id + '_btn" class="btn btn-mini form-btn" ng-click="add(this)">' +
+                                    '<i class="icon-plus"></i> Add' +
+                                    '</button>' +
                                     '</div>';
 
                             } else {
@@ -83,13 +83,13 @@ angular.module('formsAngular.form', [])
                                     template += generateLabel(info, ' <i id="add_' + info.id + ' " ng-click="add(this)" class="icon-plus-sign"></i>') +
                                         '<div class="controls" id="' + info.id + 'List" ng-repeat="arrayItem in record.' + info.name + '">' +
                                         generateInput(info, "arrayItem.x", true, null) +
-                                        '<i ng-click="remove(this,$index)" class="icon-minus-sign"></i>'+
+                                        '<i ng-click="remove(this,$index)" class="icon-minus-sign"></i>' +
                                         '</div>';
                                 } else {
                                     // Single fields here
                                     template += generateLabel(info) +
                                         '<div class="controls">' +
-                                        generateInput(info, null, attrs.required, info.id)+
+                                        generateInput(info, null, attrs.required, info.id) +
                                         '</div>';
                                 }
                             }
@@ -99,15 +99,15 @@ angular.module('formsAngular.form', [])
 
 //                        // without the "if" below I was sometimes getting the inputs repeated
 //                        if ($('#'+attrs.field).length == 0) {
-                            var info = JSON.parse(attrs.info);
-                            var template = handleField(info);
+                        var info = JSON.parse(attrs.info);
+                        var template = handleField(info);
 
-                            element.replaceWith($compile(template)(scope));
+                        element.replaceWith($compile(template)(scope));
 
-                            if (scope.updateDataDependentDisplay) {
-                                // If this is not a test force the data dependent updates to the DOM
-                                scope.updateDataDependentDisplay(scope.record, null, true);
-                            }
+                        if (scope.updateDataDependentDisplay) {
+                            // If this is not a test force the data dependent updates to the DOM
+                            scope.updateDataDependentDisplay(scope.record, null, true);
+                        }
 //                        }
                     });
                 };
@@ -118,22 +118,133 @@ angular.module('formsAngular.form', [])
     .directive('formButtons', function ($compile) {
         return {
             restrict: 'A',
-            compile: function(){
+            compile: function () {
                 return function ($scope, $element) {
-                    var template = '<div class="btn-group pull-right">'+
-                        '<button id="saveButton" class="btn btn-mini btn-primary form-btn" ng-click="save()" '+
+                    var template = '<div class="btn-group pull-right">' +
+                        '<button id="saveButton" class="btn btn-mini btn-primary form-btn" ng-click="save()" ' +
                         'ng-disabled="isSaveDisabled()"><i class="icon-ok"></i> Save</button>' +
                         '<button id="cancelButton" class="btn btn-mini btn-warning form-btn" ng-click="cancel()" ' +
-                        'ng-disabled="isCancelDisabled()"><i class="icon-remove"></i> Cancel</button>'+
-                        '</div>'+
-                        '<div class="btn-group btn-group pull-right">'+
-                        '<button id="newButton" class="btn btn-mini btn-success form-btn" ng-click="new()">'+
+                        'ng-disabled="isCancelDisabled()"><i class="icon-remove"></i> Cancel</button>' +
+                        '</div>' +
+                        '<div class="btn-group btn-group pull-right">' +
+                        '<button id="newButton" class="btn btn-mini btn-success form-btn" ng-click="new()">' +
                         '<i class="icon-plus"></i> New</button>' +
                         '<button id="deleteButton" class="btn btn-mini btn-danger form-btn" ng-click="delete()">' +
-                        '<i class="icon-minus"></i> Delete</button>'+
+                        '<i class="icon-minus"></i> Delete</button>' +
                         '</div>';
                     $element.replaceWith($compile(template)($scope));
                 }
             }
         }
+    })
+
+
+// Directive to handle subdocs.  Mostly a copy of ng-repeat, but needed to simplify it a bit to make it work
+    .directive('ngSubdocRepeat', function () {
+        return {
+            transclude: 'element',
+            priority: 1000,
+            terminal: true,
+            compile: function (element, attr, linker) {
+                return function (scope, iterStartElement, attr) {
+                    var expression = attr.ngSubdocRepeat;
+                    var match = expression.match(/^\s*(.+)\s+in\s+(.*)\s*$/),
+                        lhs, rhs, valueIdent, keyIdent;
+                    if (!match) {
+                        throw Error("Expected ngSubdocRepeat in form of '_item_ in _collection_' but got '" +
+                            expression + "'.");
+                    }
+                    lhs = match[1];
+                    rhs = match[2];
+                    match = lhs.match(/^(?:([\$\w]+)|\(([\$\w]+)\s*,\s*([\$\w]+)\))$/);
+                    if (!match) {
+                        throw Error("'item' in 'item in collection' should be identifier or (key, value) but got '" +
+                            lhs + "'.");
+                    }
+                    valueIdent = match[3] || match[1];
+                    keyIdent = match[2];
+
+                    // Store a list of elements from previous run - an array of objects with following properties:
+                    //   - scope: bound scope
+                    //   - element: previous element.
+                    var lastOrderArr = [];
+
+                    scope.$watch(function ngSubdocRepeatWatch(scope) {
+                        var index, length,
+                            collection = scope.$eval(rhs),
+                            cursor = iterStartElement,     // current position of the node
+                        // Same as lastOrder but it has the current state. It will become the
+                        // lastOrder on the next iteration.
+                            nextOrderArr = [],
+                            arrayLength,
+                            childScope,
+                            key, value, // key/value of iteration
+                            array,
+                            last;       // last object information {scope, element, index}
+                        if (!angular.isArray(collection)) {
+                            // if object, extract keys, sort them and use to determine order of iteration over obj props
+                            array = [];
+                            for (key in collection) {
+                                if (collection.hasOwnProperty(key) && key.charAt(0) != '$') {
+                                    array.push(key);
+                                }
+                            }
+                            array.sort();
+                        } else {
+                            array = collection || [];
+                        }
+
+                        arrayLength = array.length;
+
+                        // we are not using forEach for perf reasons (trying to avoid #call)
+                        for (index = 0, length = array.length; index < length; index++) {
+                            key = (collection === array) ? index : array[index];
+                            value = collection[key];
+
+                            last = lastOrderArr.shift();
+
+                            if (last) {
+                                // if we have already seen this object, then we need to reuse the
+                                // associated scope/element
+                                childScope = last.scope;
+                                nextOrderArr.push(last);
+                                cursor = last.element;
+                            } else {
+                                // new item which we don't know about
+                                childScope = scope.$new();
+                            }
+
+                            childScope[valueIdent] = value;
+                            if (keyIdent) childScope[keyIdent] = key;
+                            childScope.$index = index;
+
+                            childScope.$first = (index === 0);
+                            childScope.$last = (index === (arrayLength - 1));
+                            childScope.$middle = !(childScope.$first || childScope.$last);
+
+                            if (!last) {
+                                linker(childScope, function (clone) {
+                                    cursor.after(clone);
+                                    last = {
+                                        scope: childScope,
+                                        element: (cursor = clone),
+                                        index: index
+                                    };
+                                    nextOrderArr.push(last);
+                                });
+                            }
+                        }
+
+                        //shrink children
+                        for (var j = 0; j < lastOrderArr.length ; j++) {
+                            lastOrderArr[j].element.remove();
+                            lastOrderArr[j].scope.$destroy();
+                        }
+
+                        lastOrderArr = nextOrderArr;
+                    });
+                };
+            }
+        }
     });
+
