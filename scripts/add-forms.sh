ad821d3cee47fa6f72175c187e258c60dad82782
Mark Chapman <mark.chapman@gmail.com>
2013-04-12 16:26:14 +0100

    Add searchbox controller to list of copied files

--- scripts/add-forms.sh
+++ scripts/add-forms.sh
@@ -7,6 +7,7 @@ if [ $1 ]; then
 		# Upgrade these files
 	    cp $BASE_DIR/../app/js/controllers/base.js $BASE_DIR/../../$1/app/js/controllers/base.js
 	    cp $BASE_DIR/../app/js/controllers/model.js $BASE_DIR/../../$1/app/js/controllers/model.js
+	    cp $BASE_DIR/../app/js/controllers/searchbox.js $BASE_DIR/../../$1/app/js/controllers/searchbox.js
 	    cp $BASE_DIR/../app/js/directives/form.js $BASE_DIR/../../$1/app/js/directives/form.js
 	    cp $BASE_DIR/../app/lib/angular/angular.js $BASE_DIR/../../$1/app/lib/angular/angular.js
 	    cp $BASE_DIR/../app/css/forms-ng.css $BASE_DIR/../../$1/app/css/forms-ng.css
