d6c2cd9bdc27762d5d539dfe662890bf873e0ad5
Mark Chapman <mark.chapman@gmail.com>
2013-04-12 14:17:01 +0100

    Add the test data to git (which will hopefully make the tests work!)

---
+++ test/e2e/data/a_unadorned_mongoose.js
@@ -0,0 +1,16 @@
+[
+    {
+        surname: "TestPerson1",
+        forename: "John",
+        weight: 124,
+        eyeColour: "Blue",
+        accepted: false
+    },
+    {
+        surname: "TestPerson2",
+        forename: "Andrew",
+        weight: 142,
+        eyeColour: "Brown",
+        accepted: true
+    }
+]
---
+++ test/e2e/data/b_using_options.js
@@ -0,0 +1,28 @@
+[
+    {
+        surname: "NotAccepted",
+        forename: "John",
+        address: {
+            line1: "4 High Street",
+            town: "Anytown",
+            postcode: "AB2 3ES"},
+        email: "someone@somewhere.com",
+        weight: 124,
+        accepted: false,
+        interviewScore: 89,
+        freeText: "Here is some tex blah blah"
+    },
+    {
+        surname: "IsAccepted",
+        forename: "John",
+        address: {
+            line1: "4 High Street",
+            town: "Anytown",
+            postcode: "AB2 3ES"},
+        email: "someone@somewhere.com",
+        weight: 124,
+        accepted: true,
+        interviewScore: 89,
+        freeText: "Here is some tex blah blah"
+    }
+]
