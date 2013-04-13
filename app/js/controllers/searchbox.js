b44aacc2330523a5458c6c39157f00852648fe9c
Mark Chapman <mark.chapman@gmail.com>
2013-04-13 16:59:07 +0100

    Search box styling

--- app/js/controllers/searchbox.js
+++ app/js/controllers/searchbox.js
@@ -7,13 +7,14 @@ var searchCtrl = function ($scope, $http, $location) {
         if (newValue && newValue.length > 0) {
             console.log(newValue);
             $http.get('api/search?q=' + newValue).success(function (data) {
-                console.log(data);
                 $scope.results = data.results;
                 $scope.moreCount = data.moreCount;
+                $scope.errorClass = $scope.results.length === 0 ? "error" : "";
             }).error(function () {
                 console.log("an error happened");
             });
         } else {
+            $scope.errorClass = "";
             $scope.results = [];
         }
     },true);
