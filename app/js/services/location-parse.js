f1024a0fa6d9085c60a6e8cc18302c15603c278d
Mark Chapman <mark.chapman@gmail.com>
2013-06-07 17:05:59 +0100

    Add locationParse tests and fix bug in /model/form

--- app/js/services/location-parse.js
+++ app/js/services/location-parse.js
@@ -9,25 +9,20 @@ formsAngular.factory('$locationParse', [function() {
 
             if (location !== lastRoute) {
                 lastRoute = location;
-                lastObject = {};
+                lastObject = {newRecord: false};
                 var locationSplit = location.split('/');
                 var locationParts = locationSplit.length;
-                var lastPart = locationSplit[locationParts - 1];
-                lastObject.modelName = lastObject.formName = lastObject.id = undefined;
-                lastObject.newRecord = lastObject.newRecord || (lastPart === 'new');
                 lastObject.modelName = locationSplit[1];
-
-                if (lastObject.newRecord) {
-                    if (locationParts === 4) {
-                        lastObject.formName = locationSplit[2];
-                    }
-                } else {
-                    if (locationParts === 5) {
-                        lastObject.formName = locationSplit[2];
-                        lastObject.id = locationSplit[3];
-                    } else {
-                        lastObject.id = locationSplit[2];
-                    }
+                var lastPart = locationSplit[locationParts - 1];
+                if (lastPart === "new") {
+                    lastObject.newRecord = true;
+                    locationParts--;
+                } else if (lastPart === "edit") {
+                    locationParts = locationParts - 2
+                    lastObject.id = locationSplit[locationParts];
+                }
+                if (locationParts > 2) {
+                   lastObject.formName = locationSplit[2];
                 }
             }
             return lastObject;
--- test/unit/locationParseSpec.js
+++ test/unit/locationParseSpec.js
@@ -4,20 +4,55 @@ describe("Unit: Testing Services", function() {
 
     beforeEach(angular.mock.module('formsAngular'));
 
-    it('should contain a working $locationParse service', inject(function($locationParse) {
+    describe("$locationParse", function() {
+        it('should parse model/id/edit', inject(function($locationParse) {
+            var parsedObject = $locationParse('/b_using_options/5194c43185810b1e947cd8c2/edit');
+            expect(parsedObject.modelName).toBe("b_using_options");
+            expect(parsedObject.id).toBe("5194c43185810b1e947cd8c2");
+            expect(parsedObject.newRecord).toBe(false);
+            expect(parsedObject.formName).toBe(undefined);
+        }));
 
-        var parsedObject = $locationParse('/b_using_options/5194c43185810b1e947cd8c2/edit');
-        expect(parsedObject.modelName).toBe("b_using_options");
-        expect(parsedObject.id).toBe("5194c43185810b1e947cd8c2");
-        expect(parsedObject.newRecord).toBe(false);
-        expect(parsedObject.formName).toBe(undefined);
+        it('should parse model/new', inject(function($locationParse) {
+            var parsedObject = $locationParse('/mymodel/new');
+            expect(parsedObject.modelName).toBe("mymodel");
+            expect(parsedObject.id).toBe(undefined);
+            expect(parsedObject.newRecord).toBe(true);
+            expect(parsedObject.formName).toBe(undefined);
+        }));
 
-        parsedObject = $locationParse('/mymodel/myform/new');
-        expect(parsedObject.modelName).toBe("mymodel");
-        expect(parsedObject.id).toBe(undefined);
-        expect(parsedObject.newRecord).toBe(true);
-        expect(parsedObject.formName).toBe("myform");
+        it('should parse model', inject(function($locationParse) {
+            var parsedObject = $locationParse('/mymodel');
+            expect(parsedObject.modelName).toBe("mymodel");
+            expect(parsedObject.id).toBe(undefined);
+            expect(parsedObject.newRecord).toBe(false);
+            expect(parsedObject.formName).toBe(undefined);
+        }));
 
-    }));
+        it('should parse model/form/id/edit', inject(function($locationParse) {
+            var parsedObject = $locationParse('/b_using_options/myform/5194c43185810b1e947cd8c2/edit');
+            expect(parsedObject.modelName).toBe("b_using_options");
+            expect(parsedObject.id).toBe("5194c43185810b1e947cd8c2");
+            expect(parsedObject.newRecord).toBe(false);
+            expect(parsedObject.formName).toBe("myform");
+        }));
+
+        it('should parse model/form/new', inject(function($locationParse) {
+            var parsedObject = $locationParse('/mymodel/myform/new');
+            expect(parsedObject.modelName).toBe("mymodel");
+            expect(parsedObject.id).toBe(undefined);
+            expect(parsedObject.newRecord).toBe(true);
+            expect(parsedObject.formName).toBe("myform");
+        }));
+
+        it('should parse model/form', inject(function($locationParse) {
+            var parsedObject = $locationParse('/mymodel/myform');
+            expect(parsedObject.modelName).toBe("mymodel");
+            expect(parsedObject.id).toBe(undefined);
+            expect(parsedObject.newRecord).toBe(false);
+            expect(parsedObject.formName).toBe("myform");
+        }));
+
+    })
 
 });
