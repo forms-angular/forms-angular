486c98e6c0035588f8c1c9bbfc112ee723820938
Mark Chapman <mark.chapman@gmail.com>
2013-07-31 21:57:59 +0100

    WIP

--- server/models/c_subdoc_example.js
+++ server/models/c_subdoc_example.js
@@ -8,9 +8,11 @@ var CSchema = new Schema({
     hairColour: {type: String, enum:['Black','Brown','Blonde','Bald'], required: true, form:{placeHolder:"Select hair colour (required)", select2: {}}},  // Required combo has appropriate styling
     dateOfBirth: Date,
     accepted: Boolean,
+    passwordHash: {type:String, secure:true, forms:{hidden:true}},
     interview: {
         score:{type:Number},
-        date:{type:Date}
+        date:{type:Date},
+        interviewHash: {type:String, secure:true, forms:{hidden:true}}
     }
 });
 
---
+++ test/e2edata/c_subdoc_example.js
@@ -0,0 +1,16 @@
+[
+    {
+        _id: ObjectId("519aaaaab320153869b175e0"),
+        surname: "Anderson",
+        forename:  "John",
+        weight: 123,
+        hairColour: 'Brown',
+        accepted: true,
+        passwordHash: "top secret",
+        interview: {
+            score:98,
+            date: "23 Mar 2013",
+            interviewHash: "you think I would tell you?"
+        }
+    }
+]
