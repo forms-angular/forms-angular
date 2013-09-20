5e24cf427bfb041741e99afaabb378fb091f422a
Steve Clements <steve@originaleye.co.uk>
2013-09-20 11:57:54 +0100

    changed hide-on-empty directive to reflect changes in drag drop functionality

--- app/js/directives/hide-on-empty.js
+++ app/js/directives/hide-on-empty.js
@@ -1,21 +1,47 @@
-// 'use strict';
-
 formsAngular
 
 .directive('hideOnEmpty', function() {
-	return {
-		
-		replace: false,
+    return {
+        
+        replace: false,
 
-		link: function (scope, element, attrs) {
+        link: function (scope, element, attrs) {
 
-			if (scope.$eval(attrs.ngModel) === undefined) {
+            //if its a hide group, then its not the group but the input
 
-				element.hide();
+            if (element.hasClass('control-group')) {
 
-			}
+                if (element.find('input').length > 0) {
 
-		}
+                    if (scope.$eval(element.find('input')[0].attributes.getNamedItem('ng-model').value) === undefined) {
 
-	};
-})
+                        element.hide();
+
+                    }
+
+                }
+
+                if (element.find('textarea').length > 0) {
+
+                    if (scope.$eval(element.find('textarea')[0].attributes.getNamedItem('ng-model').value) === undefined) {
+
+                        element.hide();
+
+                    }
+
+                }
+
+                
+
+            } else {
+
+                if (scope.$eval(attrs.ngModel) === undefined) {
+
+                    element.hide();
+
+                }
+            }
+        }
+
+    };
+});
