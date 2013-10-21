8a6113c1d00fc93e236caf3f811e5cdb592051dc
Mark Chapman <mark.chapman@gmail.com>
2013-10-21 09:16:41 +0100

    Made a start

--- app/demo/demo.js
+++ app/demo/demo.js
@@ -6,6 +6,11 @@ myDemoApp.config(['$routeProvider', function ($routeProvider) {
 
         $routeProvider.
             when('/index', {templateUrl: 'partials/howto.html'}).
+            when('/get-started', {templateUrl: 'partials/get-started.html'}).
+            when('/forms', {templateUrl: 'partials/forms.html'}).
+            when('/schemas', {templateUrl: 'partials/schemas.html'}).
+            when('/reports', {templateUrl: 'partials/reports.html'}).
+            when('/more', {templateUrl: 'partials/more.html'}).
             when('/build-app', {templateUrl: 'partials/build-app.html'}).
             when('/404', {templateUrl: 'partials/404.html'}).
             when('/z_custom_form/new', {templateUrl: 'partials/custom-new.html'}).            // example view override
