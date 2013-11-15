e7ed14507898f97a42067c51a69e833295393e4b
Mark Chapman <mark.chapman@gmail.com>
2013-11-15 19:31:30 +0000

    More changes to quick start script

--- app/partials/get-started/get-started.sh
+++ app/partials/get-started/get-started.sh
@@ -2,12 +2,18 @@
 
 wget https://raw.github.com/mchapman/forms-angular/dev/app/partials/get-started/get-started-create.sh
 bash get-started-create.sh
+echo "Sub script complete...."
+pwd
+cd myapp
+pwd
+
 wget https://raw.github.com/mchapman/forms-angular/dev/app/partials/get-started/adds-app.js
 cat app.js adds-app.js > tempapp.js
 mv tempapp.js app.js
-sed -i s_app.get_//app.get_ app.js
+sed -i s_^app.get_//app.get_ app.js
 wget https://raw.github.com/mchapman/forms-angular/dev/app/partials/get-started/index.html -O public/index.html
 wget https://raw.github.com/mchapman/forms-angular/dev/app/partials/get-started/myapp.js -O public/myapp.js
+mkdir public/partials
 wget https://raw.github.com/mchapman/forms-angular/dev/app/partials/get-started/partials-index.html -O public/partials/index.html
 wget https://raw.github.com/mchapman/forms-angular/dev/app/partials/base-edit.html -O public/partials/base-edit.html
 wget https://raw.github.com/mchapman/forms-angular/dev/app/partials/base-list.html -O public/partials/base-list.html
