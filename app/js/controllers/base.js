0b3b8253bc66c648858e16048a2c80455af30e32
Steve Clements <steve@originaleye.co.uk>
2013-12-06 14:14:12 +0000

    path with selected tab

--- app/js/controllers/base.js
+++ app/js/controllers/base.js
@@ -513,6 +513,20 @@ formsAngular.controller('BaseCtrl', ['$scope', '$routeParams', '$location', '$ht
                 }
             }
         }
+        //if a hash is defined then make that the selected tab is displayed
+        if ($scope.panes.length > 0 && $location.hash()) {
+            var pane = _.find($scope.panes, function (aPane) {
+                return aPane.title === $location.hash();
+            });
+
+            if (pane) {
+                for (var i = 0; i < $scope.panes.length; i++) {
+                    $scope.panes[i].active = false;
+                }
+                pane.active = true;
+            }
+        }
+
         if (destList && destList.length === 0) {
             handleEmptyList(description, destList, destForm, source);
         }
