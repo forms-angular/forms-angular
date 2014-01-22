66c64fc30392d0c045885ac1cc1d7a9f638d9374
Mark Chapman <mark.chapman@gmail.com>
2014-01-22 01:28:58 +0000

    Improve titlecase and add tests

--- app/js/filters/titlecase.js
+++ app/js/filters/titlecase.js
@@ -1,10 +1,16 @@
 formsAngular.filter('titleCase',[function() {
     return function(str, stripSpaces) {
-        var value = str.replace(/(_|\.)/g, ' ').replace(/[A-Z]/g, ' $&').replace(/\w\S*/g, function (txt) {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-        });
+        var value = str
+            .replace(/(_|\.)/g, ' ')                       // replace underscores and dots with spaces
+            .replace(/[A-Z]/g, ' $&').trim()               // precede replace caps with a space
+            .replace(/\w\S*/g, function (txt) {            // capitalise first letter of word
+                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            });
         if (stripSpaces) {
             value = value.replace(/\s/g,'');
+        } else {
+            // lose double spaces
+            value = value.replace(/\s{2,}/g,' ');
         }
         return value;
     }
