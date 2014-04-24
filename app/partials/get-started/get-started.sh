b9afe75db3ddb5e11d0a241120ada4c6a1dccc9b
Mark Chapman <mark.chapman@gmail.com>
2014-04-24 12:31:03 +0100

    Fix problem with install script

--- app/partials/get-started/get-started.sh
+++ app/partials/get-started/get-started.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-if [! $1 ]; then
-	$1 = "0.2"
+if [ $1 ]; then
+	branch="$1"
+else
+	branch="0.2"
 fi
-wget https://raw.github.com/mchapman/forms-angular/$1/app/partials/get-started/get-started-create.sh
+
+echo Setting up example app for branch $branch
+wget https://raw.github.com/mchapman/forms-angular/$branch/app/partials/get-started/get-started-create.sh
 bash get-started-create.sh
 echo "Sub script complete...."
 pwd
 cd myapp
 pwd
 
-wget https://raw.github.com/mchapman/forms-angular/$1/app/partials/get-started/adds-app.js
+wget https://raw.github.com/mchapman/forms-angular/$branch/app/partials/get-started/adds-app.js
 cat app.js adds-app.js > tempapp.js
 mv tempapp.js app.js
 sed -i s_^app.get_//app.get_ app.js
-wget https://raw.github.com/mchapman/forms-angular/$1/app/partials/get-started/index.html -O public/index.html
-wget https://raw.github.com/mchapman/forms-angular/$1/app/partials/get-started/myapp.js -O public/myapp.js
+wget https://raw.github.com/mchapman/forms-angular/$branch/app/partials/get-started/index.html -O public/index.html
+wget https://raw.github.com/mchapman/forms-angular/$branch/app/partials/get-started/myapp.js -O public/myapp.js
 mkdir public/partials
-wget https://raw.github.com/mchapman/forms-angular/$1/app/partials/get-started/partials-index.html -O public/partials/index.html
-wget https://raw.github.com/mchapman/forms-angular/$1/app/partials/base-edit.html -O public/partials/base-edit.html
-wget https://raw.github.com/mchapman/forms-angular/$1/app/partials/base-list.html -O public/partials/base-list.html
-wget https://raw.github.com/mchapman/forms-angular/$1/app/partials/base-analysis.html -O public/partials/base-analysis.html
+wget https://raw.github.com/mchapman/forms-angular/$branch/app/partials/get-started/partials-index.html -O public/partials/index.html
+wget https://raw.github.com/mchapman/forms-angular/$branch/app/partials/base-edit.html -O public/partials/base-edit.html
+wget https://raw.github.com/mchapman/forms-angular/$branch/app/partials/base-list.html -O public/partials/base-list.html
+wget https://raw.github.com/mchapman/forms-angular/$branch/app/partials/base-analysis.html -O public/partials/base-analysis.html
 node app.js
