f64a6caf4356a0ee877a9d0f3eb05b5cbaa838b9
Mark Chapman <mark.chapman@gmail.com>
2014-04-24 12:14:07 +0100

    Working towards supporting two frameworks - ready to test get-started

--- app/partials/get-started/get-started.sh
+++ app/partials/get-started/get-started.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-wget https://raw.github.com/mchapman/forms-angular/master/app/partials/get-started/get-started-create.sh
+if [! $1 ]; then
+	$1 = "0.2"
+fi
+wget https://raw.github.com/mchapman/forms-angular/$1/app/partials/get-started/get-started-create.sh
 bash get-started-create.sh
 echo "Sub script complete...."
 pwd
 cd myapp
 pwd
 
-wget https://raw.github.com/mchapman/forms-angular/master/app/partials/get-started/adds-app.js
+wget https://raw.github.com/mchapman/forms-angular/$1/app/partials/get-started/adds-app.js
 cat app.js adds-app.js > tempapp.js
 mv tempapp.js app.js
 sed -i s_^app.get_//app.get_ app.js
-wget https://raw.github.com/mchapman/forms-angular/master/app/partials/get-started/index.html -O public/index.html
-wget https://raw.github.com/mchapman/forms-angular/master/app/partials/get-started/myapp.js -O public/myapp.js
+wget https://raw.github.com/mchapman/forms-angular/$1/app/partials/get-started/index.html -O public/index.html
+wget https://raw.github.com/mchapman/forms-angular/$1/app/partials/get-started/myapp.js -O public/myapp.js
 mkdir public/partials
-wget https://raw.github.com/mchapman/forms-angular/master/app/partials/get-started/partials-index.html -O public/partials/index.html
-wget https://raw.github.com/mchapman/forms-angular/master/app/partials/base-edit.html -O public/partials/base-edit.html
-wget https://raw.github.com/mchapman/forms-angular/master/app/partials/base-list.html -O public/partials/base-list.html
-wget https://raw.github.com/mchapman/forms-angular/master/app/partials/base-analysis.html -O public/partials/base-analysis.html
+wget https://raw.github.com/mchapman/forms-angular/$1/app/partials/get-started/partials-index.html -O public/partials/index.html
+wget https://raw.github.com/mchapman/forms-angular/$1/app/partials/base-edit.html -O public/partials/base-edit.html
+wget https://raw.github.com/mchapman/forms-angular/$1/app/partials/base-list.html -O public/partials/base-list.html
+wget https://raw.github.com/mchapman/forms-angular/$1/app/partials/base-analysis.html -O public/partials/base-analysis.html
 node app.js
--- scripts/framework.sh
+++ scripts/framework.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function Clear () {
+function Clear {
     echo Clearing out bower component directories
 	rm -rf app/bower_components/bootstrap
 	mkdir app/bower_components/bootstrap
@@ -10,35 +10,63 @@ function Clear () {
 	mkdir app/bower_components/angular-ui-bootstrap
 }
 
+function InstallBootstrap {
+	if [ -d "app/bower_components/bootstrap-$1" ]; then
+		echo Using existing copy of Bootstrap $1
+		cp -r app/bower_components/bootstrap-$1/* app/bower_components/bootstrap
+	else
+		echo Downloading and installing Bootstrap $1
+		bower install bootstrap\#$1
+		mkdir app/bower_components/bootstrap-$1
+		cp -r app/bower_components/bootstrap/* app/bower_components/bootstrap-$1
+	fi
+}
+
+# $1 = bower name  $2 = version
+# We will build from directory angular-ui-bootstrap, which no version is named (I think)
+function InstallUIBootstrap {
+	if [ -d "app/bower_components/$1-$2" ]; then
+		echo Using existing copy of Bootstrap UI $2
+		cp -r app/bower_components/$1-$2/* app/bower_components/angular-ui-bootstrap
+	else
+		echo Downloading and installing Bootstrap UI $2
+		bower install $1\#$2
+		mkdir app/bower_components/$1-$2
+		cp -r app/bower_components/$1/* app/bower_components/$1-$2
+	fi
+}
+
+function InstallSelect2CSS {
+	if [ -d "app/bower_components/select2-bootstrap-css-$1" ]; then
+		echo Using existing copy of select2-bootstrap-css $1
+		cp -r app/bower_components/select2-bootstrap-css-$1/* app/bower_components/select2-bootstrap-css
+	else
+		echo Downloading and installing select2-bootstrap-css $1
+		bower install select2-bootstrap-css\#$1
+		mkdir app/bower_components/select2-bootstrap-css-$1
+		cp -r app/bower_components/select2-bootstrap-css/* app/bower_components/select2-bootstrap-css-$1
+	fi
+}
+
 Clear
 if [ $1 ]; then
 	case "$1" in
 		"bs2" )
-		    echo Copying in bs2 variants
-			cp -r app/bower_components/bootstrap-2.3.2/* app/bower_components/bootstrap
-			cp -r app/bower_components/select2-bootstrap-css-1.2.0/* app/bower_components/select2-bootstrap-css
-			cp -r app/bower_components/angular-ui-bootstrap-bower/* app/bower_components/angular-ui-bootstrap
-			cp app/css/bootswatch-cerulean-2.3.2.less app/css/bootswatch.less
-			cp app/css/variables-cerulean-2.3.2.less app/css/variables.less
-			cp app/demo/css/demo-bs2.less app/demo/css/demo.less
-			cat app/css/warning-banner.less app/css/forms-angular-bs2.less > app/css/forms-angular.less
+			InstallBootstrap "2.3.2"
+			InstallUIBootstrap "angular-ui-bootstrap-bower" "0.8.0"
+			InstallSelect2CSS "1.2.0"
 			;;
 		"bs3" )
-		    echo Copying in bs3 variants
-			cp -r app/bower_components/bootstrap-3.1.1/* app/bower_components/bootstrap
-			cp -r app/bower_components/select2-bootstrap-css-1.3.0/* app/bower_components/select2-bootstrap-css
-			cp -r app/bower_components/angular-ui-bootstrap-0.10.0/* app/bower_components/angular-ui-bootstrap
-			cp app/css/bootswatch-cerulean-3.1.1.less app/css/bootswatch.less
-			cp app/css/variables-cerulean-3.1.1.less app/css/variables.less
-			cp app/demo/css/demo-bs3.less app/demo/css/demo.less
-			cat app/css/warning-banner.less app/css/forms-angular-bs3.less > app/css/forms-angular.less
+			InstallBootstrap "3.1.1"
+			InstallUIBootstrap "angular-bootstrap" "0.10.0"
+			InstallSelect2CSS "1.3.0"
 			;;
 		* )
 			echo "Invalid option $1"
 			;;
 	esac
 	echo Generating css
-	grunt less
+	grunt less:$1
 else
 	echo ""
 	echo Usage: framework [bs2, bs3]
