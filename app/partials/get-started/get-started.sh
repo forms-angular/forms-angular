642bdde0bff9ee31a734da495f6b7d267888a9ac
Mark Chapman <mark.chapman@gmail.com>
2014-04-27 06:25:59 +0100

    Move sed command to after the file is created.  Duh.

--- app/partials/get-started/get-started.sh
+++ app/partials/get-started/get-started.sh
@@ -39,15 +39,13 @@ cat app.js adds-app.js > tempapp.js
 mv tempapp.js app.js
 sed -i s_^app.get_//app.get_ app.js
 wget https://raw.github.com/mchapman/forms-angular/$branch/app/partials/get-started/index.html -O public/index.html
+wget https://raw.github.com/mchapman/forms-angular/$branch/app/partials/get-started/myapp.js -O public/myapp.js
 if [ $framework = "bs3" ]; then
 	#  use the correct css
 	sed -i s_forms-angular-bs2.css_forms-angular-bs3.css_ public/index.html
 	#  set the bs3 option
 	sed -i "s_//uncomment to use Bootstrap 3--__" public/myapp.js
 fi
-
-
-wget https://raw.github.com/mchapman/forms-angular/$branch/app/partials/get-started/myapp.js -O public/myapp.js
 mkdir public/partials
 wget https://raw.github.com/mchapman/forms-angular/$branch/app/partials/get-started/partials-index.html -O public/partials/index.html
 wget https://raw.github.com/mchapman/forms-angular/$branch/app/partials/base-edit.html -O public/partials/base-edit.html
