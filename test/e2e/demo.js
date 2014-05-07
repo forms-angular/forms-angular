97b69ee43696f19dd25a79fd696b245e2b6325d3
Mark Chapman <mark.chapman@gmail.com>
2014-05-07 20:13:28 +0100

    Everything now under jshint

--- test/e2e/demo.js
+++ test/e2e/demo.js
@@ -1,10 +1,10 @@
 'use strict';
 
-describe('Forms app demo', function() {
+describe('Forms app demo', function () {
 
-    it('should automatically redirect to index when location hash/fragment is empty', function () {
-        browser().navigateTo('/');
-        expect(browser().window().hash()).toMatch('\/index');
-    });
+  it('should automatically redirect to index when location hash/fragment is empty', function () {
+    browser().navigateTo('/');
+    expect(browser().window().hash()).toMatch('\/index');
+  });
 
 });
--- test/e2e/find_functions.js
+++ test/e2e/find_functions.js
@@ -1,19 +1,19 @@
 'use strict';
 
-describe('Find functions', function() {
+describe('Find functions', function () {
 
-    it('should find only the allowed records', function () {
-        browser().navigateTo('/#!/b_using_options');
-        expect( repeater( '.list-item' ).count() ).toEqual(2);
-        expect( element('a').text() ).toMatch(/IsAccepted/);
-        expect( element('a').text() ).not().toMatch(/NotAccepted/);
-    });
+  it('should find only the allowed records', function () {
+    browser().navigateTo('/#!/b_using_options');
+    expect(repeater('.list-item').count()).toEqual(2);
+    expect(element('a').text()).toMatch(/IsAccepted/);
+    expect(element('a').text()).not().toMatch(/NotAccepted/);
+  });
 
-    it('should support filters', function () {
-        browser().navigateTo('/#!/a_unadorned_mongoose?f=%7B%22eyeColour%22:%22Blue%22%7D');
-        expect( repeater( '.list-item' ).count() ).toEqual(1);
-        expect( element('a').text() ).toMatch(/TestPerson1/);
-        expect( element('a').text() ).not().toMatch(/TestPerson2/);
-    });
+  it('should support filters', function () {
+    browser().navigateTo('/#!/a_unadorned_mongoose?f=%7B%22eyeColour%22:%22Blue%22%7D');
+    expect(repeater('.list-item').count()).toEqual(1);
+    expect(element('a').text()).toMatch(/TestPerson1/);
+    expect(element('a').text()).not().toMatch(/TestPerson2/);
+  });
 
 });
