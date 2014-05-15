7eb6de68764d840895c8ee7fa3c664c858a52229
Mark Chapman <mark.chapman@gmail.com>
2014-05-15 17:57:44 +0100

    Make getting started more testable

--- app/partials/get-started/get-started-create.sh
+++ app/partials/get-started/get-started-create.sh
@@ -5,8 +5,16 @@ cd myapp
 express
 npm install
 
-npm install forms-angular --save
+if [ $1 ]; then
+  npm install forms-angular-test --save
+else
+  npm install forms-angular --save
+fi
 npm install mongoose --save
 cd public
-bower install forms-angular --allow-root
+if [ $1 ]; then
+  bower install forms-angular#dev --allow-root
+else
+  bower install forms-angular --allow-root
+fi
 cd ..
