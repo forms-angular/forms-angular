e9c48f70e44b8e332402a0320e921e9f26b2bdd1
Dominic Böttger <Dominic.Boettger@inspirationlabs.com>
2014-05-21 17:14:42 +0200

    ui.router for tests
    Signed-off-by: Dominic Böttger <Dominic.Boettger@inspirationlabs.com>

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -8,6 +8,7 @@ module.exports = function(config) {
             "app/bower_components/angular/angular.js",
             "app/bower_components/angular-sanitize/angular-sanitize.js",
             "app/bower_components/angular-route/angular-route.js",
+            "app/bower_components/angular-ui-router/release/angular-ui-router.js",
             "app/bower_components/underscore/underscore.js",
             "app/bower_components/angular-ui-bootstrap-bower/ui-bootstrap-tpls.js",
             "app/bower_components/angular-ui-date/src/date.js",
