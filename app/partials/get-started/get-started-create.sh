5d7455dac4c9ed81dac491b7b05c941eb07150f8
Mark Chapman <mark.chapman@gmail.com>
2014-05-22 12:25:22 +0100

    Remove bash script nonsense from  getting started

--- app/partials/get-started/get-started-create.sh
+++ app/partials/get-started/get-started-create.sh
@@ -1,20 +1,10 @@
-#!/bin/sh
-
 mkdir myapp
 cd myapp
 express
 npm install
 
-if [ $1 ]; then
-  npm install forms-angular-test --save
-else
-  npm install forms-angular --save
-fi
+npm install forms-angular --save
 npm install mongoose --save
 cd public
-if [ $1 ]; then
-  bower install forms-angular#dev --allow-root
-else
-  bower install forms-angular --allow-root
-fi
+bower install forms-angular
 cd ..
