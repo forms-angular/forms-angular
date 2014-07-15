bf306abb721faa2bb538638ba84b1ecba3eb797a
Mark Chapman <mark.chapman@gmail.com>
2014-07-15 16:20:01 +0100

    Split functions needed outside NarBar out of NavCtrl into new FngCtrl so NavPar can be split out into a partial with the controller.

--- js/controllers/nav.js
+++ js/controllers/nav.js
@@ -1,28 +1,10 @@
 'use strict';
 
 formsAngular.controller('NavCtrl',
-  ['$scope', '$data', '$location', '$filter', '$locationParse', '$controller', 'urlService', 'cssFrameworkService',
-    function ($scope, $data, $location, $filter, $locationParse, $controller, urlService, cssFrameworkService) {
+  ['$scope', '$data', '$location', '$filter', '$locationParse', '$controller', 'urlService',
+    function ($scope, $data, $location, $filter, $locationParse, $controller, urlService) {
 
   $scope.items = [];
-
-  $scope.globalShortcuts = function (event) {
-    if (event.keyCode === 191 && event.ctrlKey) {
-      // Ctrl+/ takes you to global search
-      document.querySelector('#searchinput').focus();
-      event.preventDefault();
-    }
-  };
-
-  $scope.css = function (fn, arg) {
-    var result;
-    if (typeof cssFrameworkService[fn] === 'function') {
-      result = cssFrameworkService[fn](arg);
-    } else {
-      result = 'error text-error';
-    }
-    return result;
-  };
 
   function loadControllerAndMenu(controllerName, level) {
     var locals = {}, addThis;
