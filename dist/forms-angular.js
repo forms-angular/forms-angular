forms-angular/forms-angular

commit d6e6734f508411649e128ea82a83e26b0db3e996

Fix (plugins)
Makes sure a plugin conversion is called when plugin is used on a subschema.

dist/forms-angular.js
@@ -1,4 +1,4 @@
-/*! forms-angular 2015-01-14 */
+/*! forms-angular 2015-01-15 */
 'use strict';
 
 var formsAngular = angular.module('formsAngular', [
@@ -2664,18 +2664,17 @@ formsAngular.factory('recordHandler', function (
         master = master || anObject;
         for (var i = 0; i < schema.length; i++) {
           var schemaEntry = schema[i];
-          var fieldname = schemaEntry.name.slice(prefixLength);
+          var fieldName = schemaEntry.name.slice(prefixLength);
+            var fieldValue = getData(anObject, fieldName);
             if (schemaEntry.schema) {
-                var extractField = getData(anObject, fieldname);
-                if (extractField) {
-                    for (var j = 0; j < extractField.length; j++) {
-                        extractField[j] = convertToAngularModel(schemaEntry.schema, extractField[j], prefixLength + 1 + fieldname.length, $scope, master);
+                if (fieldValue) {
+                    for (var j = 0; j < fieldValue.length; j++) {
+                        fieldValue[j] = convertToAngularModel(schemaEntry.schema, fieldValue[j], prefixLength + 1 + fieldName.length, $scope, master);
                     }
                 }
             } else {
-
                 // Convert {array:['item 1']} to {array:[{x:'item 1'}]}
-                var thisField = exports.getListData(anObject, fieldname, $scope.select2List);
+                var thisField = exports.getListData(anObject, fieldName, $scope.select2List);
                 if (schemaEntry.array && simpleArrayNeedsX(schemaEntry) && thisField) {
                     for (var k = 0; k < thisField.length; k++) {
                         thisField[k] = {x: thisField[k] };
@@ -2684,20 +2683,23 @@ formsAngular.factory('recordHandler', function (
 
                 // Convert {lookup:'012abcde'} to {lookup:'List description for 012abcde'}
                 var idList = $scope[exports.suffixCleanId(schemaEntry, '_ids')];
-                if (idList && idList.length > 0 && anObject[fieldname]) {
-                    anObject[fieldname] = convertForeignKeys(schemaEntry, anObject[fieldname], $scope[exports.suffixCleanId(schemaEntry, 'Options')], idList);
+                if (fieldValue && idList && idList.length > 0) {
+                    if (fieldName.indexOf('.') !== -1) {throw new Error('Trying to directly assign to a nested field 332');}  // Not sure that this can happen, but put in a runtime test
+                    anObject[fieldName] = convertForeignKeys(schemaEntry, fieldValue, $scope[exports.suffixCleanId(schemaEntry, 'Options')], idList);
                 } else if (schemaEntry.select2 && !schemaEntry.select2.fngAjax) {
-                    if (anObject[fieldname]) {
+                    if (fieldValue) {
                         if (schemaEntry.array) {
-                            for (var n = 0; n < anObject[fieldname].length; n++) {
+                            for (var n = 0; n < fieldValue.length; n++) {
                                 $scope[schemaEntry.select2.s2query].query({
-                                    term: anObject[fieldname][n].x.text || anObject[fieldname][n].text || anObject[fieldname][n].x || anObject[fieldname][n],
+                                    term: fieldValue[n].x.text || fieldValue[n].text || fieldValue[n].x || fieldValue[n],
                                     callback: function (array) {
                                         if (array.results.length > 0) {
-                                            if (anObject[fieldname][n].x) {
-                                                anObject[fieldname][n].x = array.results[0];
+                                            if (fieldValue.x) {
+                                                if (fieldName.indexOf('.') !== -1) {throw new Error('Trying to directly assign to a nested field 342');}
+                                                anObject[fieldName][n].x = array.results[0];
                                             } else {
-                                                anObject[fieldname][n] = array.results[0];
+                                              if (fieldName.indexOf('.') !== -1) {throw new Error('Trying to directly assign to a nested field 345');}
+                                              anObject[fieldName][n] = array.results[0];
                                             }
                                         }
                                     }
@@ -2705,10 +2707,11 @@ formsAngular.factory('recordHandler', function (
                             }
                         } else {
                             $scope[schemaEntry.select2.s2query].query({
-                                term: anObject[fieldname],
+                                term: fieldValue,
                                 callback: function (array) {
                                     if (array.results.length > 0) {
-                                        anObject[fieldname] = array.results[0];
+                                        if (fieldName.indexOf('.') !== -1) {throw new Error('Trying to directly assign to a nested field 357');}
+                                        anObject[fieldName] = array.results[0];
                                     }
                                 }
                             });
@@ -2717,9 +2720,9 @@ formsAngular.factory('recordHandler', function (
                 } else if (schemaEntry.select2) {
                   // Do nothing with these - handled elsewhere (and deprecated)
                   void(schemaEntry.select2);
-                } else if (anObject[fieldname] && $scope.conversions[schemaEntry.name] && $scope.conversions[schemaEntry.name].fngajax) {
+                } else if (fieldValue && $scope.conversions[schemaEntry.name] && $scope.conversions[schemaEntry.name].fngajax) {
                   var conversionEntry = schemaEntry;
-                  $scope.conversions[conversionEntry.name].fngajax(anObject[fieldname], conversionEntry, function(updateEntry, value) {
+                  $scope.conversions[conversionEntry.name].fngajax(fieldValue, conversionEntry, function(updateEntry, value) {
                     // Update the master and (preserving pristine if appropriate) the record
                     exports.setData(master, updateEntry.name, undefined, value);
                     exports.preservePristine(angular.element('#'+updateEntry.id), function() {

js/services/record-handler.js
@@ -308,18 +308,17 @@ formsAngular.factory('recordHandler', function (
         master = master || anObject;
         for (var i = 0; i < schema.length; i++) {
           var schemaEntry = schema[i];
-          var fieldname = schemaEntry.name.slice(prefixLength);
+          var fieldName = schemaEntry.name.slice(prefixLength);
+            var fieldValue = getData(anObject, fieldName);
             if (schemaEntry.schema) {
-                var extractField = getData(anObject, fieldname);
-                if (extractField) {
-                    for (var j = 0; j < extractField.length; j++) {
-                        extractField[j] = convertToAngularModel(schemaEntry.schema, extractField[j], prefixLength + 1 + fieldname.length, $scope, master);
+                if (fieldValue) {
+                    for (var j = 0; j < fieldValue.length; j++) {
+                        fieldValue[j] = convertToAngularModel(schemaEntry.schema, fieldValue[j], prefixLength + 1 + fieldName.length, $scope, master);
                     }
                 }
             } else {
-
                 // Convert {array:['item 1']} to {array:[{x:'item 1'}]}
-                var thisField = exports.getListData(anObject, fieldname, $scope.select2List);
+                var thisField = exports.getListData(anObject, fieldName, $scope.select2List);
                 if (schemaEntry.array && simpleArrayNeedsX(schemaEntry) && thisField) {
                     for (var k = 0; k < thisField.length; k++) {
                         thisField[k] = {x: thisField[k] };
@@ -328,20 +327,23 @@ formsAngular.factory('recordHandler', function (
 
                 // Convert {lookup:'012abcde'} to {lookup:'List description for 012abcde'}
                 var idList = $scope[exports.suffixCleanId(schemaEntry, '_ids')];
-                if (idList && idList.length > 0 && anObject[fieldname]) {
-                    anObject[fieldname] = convertForeignKeys(schemaEntry, anObject[fieldname], $scope[exports.suffixCleanId(schemaEntry, 'Options')], idList);
+                if (fieldValue && idList && idList.length > 0) {
+                    if (fieldName.indexOf('.') !== -1) {throw new Error('Trying to directly assign to a nested field 332');}  // Not sure that this can happen, but put in a runtime test
+                    anObject[fieldName] = convertForeignKeys(schemaEntry, fieldValue, $scope[exports.suffixCleanId(schemaEntry, 'Options')], idList);
                 } else if (schemaEntry.select2 && !schemaEntry.select2.fngAjax) {
-                    if (anObject[fieldname]) {
+                    if (fieldValue) {
                         if (schemaEntry.array) {
-                            for (var n = 0; n < anObject[fieldname].length; n++) {
+                            for (var n = 0; n < fieldValue.length; n++) {
                                 $scope[schemaEntry.select2.s2query].query({
-                                    term: anObject[fieldname][n].x.text || anObject[fieldname][n].text || anObject[fieldname][n].x || anObject[fieldname][n],
+                                    term: fieldValue[n].x.text || fieldValue[n].text || fieldValue[n].x || fieldValue[n],
                                     callback: function (array) {
                                         if (array.results.length > 0) {
-                                            if (anObject[fieldname][n].x) {
-                                                anObject[fieldname][n].x = array.results[0];
+                                            if (fieldValue.x) {
+                                                if (fieldName.indexOf('.') !== -1) {throw new Error('Trying to directly assign to a nested field 342');}
+                                                anObject[fieldName][n].x = array.results[0];
                                             } else {
-                                                anObject[fieldname][n] = array.results[0];
+                                              if (fieldName.indexOf('.') !== -1) {throw new Error('Trying to directly assign to a nested field 345');}
+                                              anObject[fieldName][n] = array.results[0];
                                             }
                                         }
                                     }
@@ -349,10 +351,11 @@ formsAngular.factory('recordHandler', function (
                             }
                         } else {
                             $scope[schemaEntry.select2.s2query].query({
-                                term: anObject[fieldname],
+                                term: fieldValue,
                                 callback: function (array) {
                                     if (array.results.length > 0) {
-                                        anObject[fieldname] = array.results[0];
+                                        if (fieldName.indexOf('.') !== -1) {throw new Error('Trying to directly assign to a nested field 357');}
+                                        anObject[fieldName] = array.results[0];
                                     }
                                 }
                             });
@@ -361,9 +364,9 @@ formsAngular.factory('recordHandler', function (
                 } else if (schemaEntry.select2) {
                   // Do nothing with these - handled elsewhere (and deprecated)
                   void(schemaEntry.select2);
-                } else if (anObject[fieldname] && $scope.conversions[schemaEntry.name] && $scope.conversions[schemaEntry.name].fngajax) {
+                } else if (fieldValue && $scope.conversions[schemaEntry.name] && $scope.conversions[schemaEntry.name].fngajax) {
                   var conversionEntry = schemaEntry;
-                  $scope.conversions[conversionEntry.name].fngajax(anObject[fieldname], conversionEntry, function(updateEntry, value) {
+                  $scope.conversions[conversionEntry.name].fngajax(fieldValue, conversionEntry, function(updateEntry, value) {
                     // Update the master and (preserving pristine if appropriate) the record
                     exports.setData(master, updateEntry.name, undefined, value);
                     exports.preservePristine(angular.element('#'+updateEntry.id), function() {

commit 4081be8439676e4eececb7050565f91214901c19

Fix (forms)
Fix regression introduced in d6e6734

dist/forms-angular.js
@@ -2694,7 +2694,7 @@ formsAngular.factory('recordHandler', function (
                                     term: fieldValue[n].x.text || fieldValue[n].text || fieldValue[n].x || fieldValue[n],
                                     callback: function (array) {
                                         if (array.results.length > 0) {
-                                            if (fieldValue.x) {
+                                            if (fieldValue[n].x) {
                                                 if (fieldName.indexOf('.') !== -1) {throw new Error('Trying to directly assign to a nested field 342');}
                                                 anObject[fieldName][n].x = array.results[0];
                                             } else {

js/services/record-handler.js
@@ -338,7 +338,7 @@ formsAngular.factory('recordHandler', function (
                                     term: fieldValue[n].x.text || fieldValue[n].text || fieldValue[n].x || fieldValue[n],
                                     callback: function (array) {
                                         if (array.results.length > 0) {
-                                            if (fieldValue.x) {
+                                            if (fieldValue[n].x) {
                                                 if (fieldName.indexOf('.') !== -1) {throw new Error('Trying to directly assign to a nested field 342');}
                                                 anObject[fieldName][n].x = array.results[0];
                                             } else {