2613fd3718faf9e8a59df5f0b3cd78c678520fc9
Mark Chapman <mark.chapman@gmail.com>
2015-03-22 01:59:03 +0000

    fix(forms) Changes behaviour of validation error reporting so that empty pristine required fields are marked as required but not errors, and other validation types are reported as errors.
    This makes required fields in a new record slightly less in your face.
    
    This is a change in behaviour from 4.1, where the has-error class was introduced.

--- test/midway/validationSpec.js
+++ test/midway/validationSpec.js
@@ -85,45 +85,74 @@ describe('validation', function () {
 
   describe('BS3 Validation Styling', function () {
 
-    describe('simple required input', function () {
+    describe('simple input', function () {
 
       beforeEach(inject(function (_$httpBackend_, $rootScope, $compile, $location, $controller, cssFrameworkService) {
         cssFrameworkService.setFrameworkForDemoWebsite('bs3');
         $httpBackend = _$httpBackend_;
         $httpBackend.whenGET('/api/schema/collection').respond({
           'surname': {
-            'enumValues': [],
-            'regExp': null,
             'path': 'surname',
             'instance': 'String',
             'validators': [[null, 'Path `{PATH}` is required.', 'required']],
-            'setters': [],
-            'getters': [],
             'options': {'index': true, 'required': true, 'list': {}},
-            '_index': true,
-            'isRequired': true,
-            '$conditionalHandlers': {}
+            '_index': true
+          },
+          'postcode': {
+            'path': 'postcode',
+            'instance': 'String',
+            'validators': [[null, 'Path `{PATH}` is invalid ({VALUE}).', 'regexp']],
+            'options': {
+              'match': '^(GIR 0AA|[A-Z]{1,2}[0-9][0-9A-Z]? [0-9][ABD-HJLNP-UW-Z]{2})$',
+              'form': {'label': 'Postcode', 'size': 'small', 'help': 'Enter your UK postcode (for example TN2 1AA)'}
+            }
           }
         });
+        $httpBackend.whenGET('/api/collection/123').respond({
+          postcode: 'invalid'
+        });
+
         elm = angular.element('<div><form-input formStyle="horizontalCompact" schema="formSchema"></form-input></div>');
         scope = $rootScope.$new();
         scope.topLevelFormName = 'baseForm';
-        $location.$$path = '/collection/new';
+        $location.$$path = '/collection/123/edit';
         ctrl = $controller('BaseCtrl', {$scope: scope});
         $httpBackend.flush();
         $compile(elm)(scope);
         scope.$digest();
       }));
 
-      it('has class has-error', function () {
-        var group = elm.find('label').parent();
-        expect(group).toHaveClass('has-error');
-        expect(group).toHaveClass('form-group');
+      it('has class has-error for pristine invalid field', function () {
+        var postcodeGroup = angular.element(elm.find('label')[1]).parent();
+        expect(postcodeGroup).toHaveClass('has-error');
+        expect(postcodeGroup).toHaveClass('form-group');
+      });
+
+      it('has class has-error for dirty required field, but not pristine', function () {
+        var surnameInput = angular.element(elm.find('input')[0]);
+        var surnameGroup = angular.element(elm.find('label')[0]).parent();
+
+        expect(surnameGroup).toHaveClass('form-group');
+        expect(surnameGroup).not.toHaveClass('has-error');
+
+        surnameInput.val('new name');
+        surnameInput.triggerHandler('change');
+        expect(surnameGroup).not.toHaveClass('has-error');
+
+        surnameInput.val('');
+        surnameInput.triggerHandler('change');
+        expect(surnameGroup).toHaveClass('has-error');
       });
 
     });
 
-    describe('nested required input', function () {
+    describe('nested input', function () {
+
+      var examsSection;
+      var body;
+      var exam;
+      var date;
+      var postcode;
 
       beforeEach(inject(function (_$httpBackend_, $rootScope, $compile, $location, $controller, cssFrameworkService) {
         cssFrameworkService.setFrameworkForDemoWebsite('bs3');
@@ -159,6 +188,15 @@ describe('validation', function () {
                   'options': {'required': true},
                   '_index': null,
                   'isRequired': true
+                },
+                'postcode': {
+                  'path': 'postcode',
+                  'instance': 'String',
+                  'validators': [[null, 'Path `{PATH}` is invalid ({VALUE}).', 'regexp']],
+                  'options': {
+                    'match': '^(GIR 0AA|[A-Z]{1,2}[0-9][0-9A-Z]? [0-9][ABD-HJLNP-UW-Z]{2})$',
+                    'form': {'label': 'Postcode', 'size': 'small', 'help': 'Enter your UK postcode (for example TN2 1AA)'}
+                  }
                 },
                 'score': {
                   'path': 'score',
@@ -199,7 +237,7 @@ describe('validation', function () {
               '$conditionalHandlers': {}
             }
           });
-        $httpBackend.whenGET('/api/collection/123').respond({surname: 'Smith', exams: [{result: 'Pass'}]});
+        $httpBackend.whenGET('/api/collection/123').respond({surname: 'Smith', exams: [{result: 'Pass', postcode:'invalid'}]});
         elm = angular.element('<div><form-input formStyle="horizontalCompact" schema="formSchema"></form-input></div>');
         scope = $rootScope.$new();
         $location.$$path = '/collection/123/edit';
@@ -207,20 +245,41 @@ describe('validation', function () {
         $httpBackend.flush();
         $compile(elm)(scope);
         scope.$digest();
+        examsSection = angular.element(elm.find('form').children())[2];
+        body = angular.element(angular.element(examsSection).children())[1];
+        exam = angular.element(angular.element(body).children())[0];
+        date = angular.element(angular.element(angular.element(exam).children())[2]);
+        postcode = angular.element(angular.element(angular.element(exam).children())[3]);
       }));
 
-      it('has class has-error in subschema', function () {
-        var examsSection = angular.element(elm.find('form').children())[2];
-        var body = angular.element(angular.element(examsSection).children())[1];
-        var exam = angular.element(angular.element(body).children())[0];
-        var date = angular.element(angular.element(angular.element(exam).children())[2]);
-        expect(date).toHaveClass('has-error');
+      it('has class has-error for pattern validation fail', function () {
+        expect(postcode).toHaveClass('has-error');
+        expect(postcode).toHaveClass('form-group');
+      });
+
+      it('has class has-error for edited required field', function () {
+        expect(date).not.toHaveClass('has-error');
         expect(date).toHaveClass('form-group');
+        var input = angular.element(date.find('input'));
+
+        input.val('01/01/1980');
+        input.triggerHandler('change');
+        expect(date).not.toHaveClass('has-error');
+
+        input.val('');
+        input.triggerHandler('change');
+        expect(date).toHaveClass('has-error');
+
       });
 
     });
 
-    describe('nested subkey required input', function () {
+    describe('nested subkey', function () {
+
+      var examsSection;
+      var body;
+      var date;
+      var postcode;
 
       beforeEach(inject(function (_$httpBackend_, $rootScope, $compile, $location, $controller, cssFrameworkService) {
         cssFrameworkService.setFrameworkForDemoWebsite('bs3');
@@ -256,6 +315,15 @@ describe('validation', function () {
                 'options': {'required': true},
                 'isRequired': true
               },
+              'postcode': {
+                'path': 'postcode',
+                'instance': 'String',
+                'validators': [[null, 'Path `{PATH}` is invalid ({VALUE}).', 'regexp']],
+                'options': {
+                  'match': '^(GIR 0AA|[A-Z]{1,2}[0-9][0-9A-Z]? [0-9][ABD-HJLNP-UW-Z]{2})$',
+                  'form': {'label': 'Postcode', 'size': 'small', 'help': 'Enter your UK postcode (for example TN2 1AA)'}
+                }
+              },
               'score': {
                 'path': 'score',
                 'instance': 'Number'
@@ -284,7 +352,7 @@ describe('validation', function () {
             }
           }
         });
-        $httpBackend.whenGET('/api/collection/123').respond({surname: 'Smith', exams: [{result: 'Pass'}]});
+        $httpBackend.whenGET('/api/collection/123').respond({surname: 'Smith', exams: [{result: 'Pass', postcode: 'invalid'}]});
         elm = angular.element('<div><form-input formStyle="horizontalCompact" schema="formSchema"></form-input></div>');
         scope = $rootScope.$new();
         $location.$$path = '/collection/123/edit';
@@ -292,14 +360,36 @@ describe('validation', function () {
         $httpBackend.flush();
         $compile(elm)(scope);
         scope.$digest();
+        examsSection = angular.element(elm.find('form').children())[2];
+        body = angular.element(angular.element(examsSection).children())[0];
+        date = angular.element(angular.element(angular.element(body).children())[1]);
+        postcode = angular.element(angular.element(angular.element(body).children())[2]);
       }));
 
-      it('has class has-error in subschema', function () {
-        var examsSection = angular.element(elm.find('form').children())[2];
-        var body = angular.element(angular.element(examsSection).children())[0];
-        var date = angular.element(angular.element(angular.element(body).children())[1]);
-        expect(date).toHaveClass('has-error');
+      it('handles required fields', function () {
+        expect(date).not.toHaveClass('has-error');
         expect(date).toHaveClass('form-group');
+        var input = angular.element(date.find('input'));
+
+        input.val('01/01/1980');
+        input.triggerHandler('change');
+        expect(date).not.toHaveClass('has-error');
+
+        input.val('');
+        input.triggerHandler('change');
+        expect(date).toHaveClass('has-error');
+      });
+
+      it('has class has-error in subschema for invalid postcode', function () {
+        var input = angular.element(postcode.find('input'));
+
+        // This edge case is really failing, but can be forced to work by doing an edit
+        input.val('01/01/1980');
+        input.triggerHandler('change');
+        // TODO
+
+        expect(postcode).toHaveClass('has-error');
+        expect(postcode).toHaveClass('form-group');
       });
 
     });
