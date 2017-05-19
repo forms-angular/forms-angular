018aa6d980a2d046b8ff90d8da04925cfd812b04
Mark Chapman <mark.chapman@gmail.com>
2017-05-19 19:37:06 +0100

    chore(deps) Missed two references to underscore

--- test/api/ControlAPISpec.js
+++ test/api/ControlAPISpec.js
@@ -6,7 +6,7 @@ var express = require('express');
 var async = require('async');
 var path = require('path');
 var fs = require('fs');
-var _ = require('underscore');
+var _ = require('lodash');
 var exec = require('child_process').exec;
 var mongoose = require('mongoose');
 
