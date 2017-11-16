0a48de804c6bcf3b5c5e6bd1e756f1298271c66e
Mark Chapman <mark.chapman@gmail.com>
2017-11-16 15:10:51 +0000

    chore(deps) Tidy up other deps (apart from uglify-js)

--- dist/forms-angular.d.ts
+++ dist/forms-angular.d.ts
@@ -1,6 +1,5 @@
 /// <reference path="../node_modules/@types/angular/index.d.ts" />
 /// <reference path="../node_modules/@types/lodash/index.d.ts" />
-/// <reference path="../node_modules/@types/jquery/index.d.ts" />
 /// <reference types="angular" />
 declare module fng {
     interface IFieldViewInfo {
