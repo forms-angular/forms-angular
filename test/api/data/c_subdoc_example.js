d930be1f26a9c0d2ad716a1b294800e91bc479ef
Mark Chapman <mark.chapman@gmail.com>
2020-07-08 20:04:30 +0000

    Convert to JSON part 1

--- test/api/data/c_subdoc_example.js
+++ test/api/data/c_subdoc_example.js
@@ -1,30 +1,30 @@
 [
     {
-        _id: ObjectId("519aaaaab320153869b175e0"),
-        surname: "Anderson",
-        forename:  "John",
-        weight: 123,
-        hairColour: 'Brown',
-        accepted: true,
-        passwordHash: "top secret",
-        interview: {
-            score:98,
-            date: "23 Mar 2013",
-            interviewHash: "you think I would tell you?"
+        "_id": ObjectId("519aaaaab320153869b175e0"),
+        "surname": "Anderson",
+        "forename":  "John",
+        "weight": 123,
+        "hairColour": 'Brown',
+        "accepted": true,
+        "passwordHash": "top secret",
+        "interview": {
+            "score":98,
+            "date": "23 Mar 2013",
+            "interviewHash": "you think I would tell you?"
         }
     },
     {
-        _id: ObjectId("519aaaaab3201fff69b175e0"),
-        surname: "Zodiac",
-        forename:  "Alison",
-        weight: 115,
-        hairColour: 'Black',
-        accepted: true,
-        passwordHash: "top secret",
-        interview: {
-            score:67,
-            date: "28 Mar 2013",
-            interviewHash: "you think I would tell you?"
+        "_id": ObjectId("519aaaaab3201fff69b175e0"),
+        "surname": "Zodiac",
+        "forename":  "Alison",
+        "weight": 115,
+        "hairColour": 'Black',
+        "accepted": true,
+        "passwordHash": "top secret",
+        "interview": {
+            "score":67,
+            "date": "28 Mar 2013",
+            "interviewHash": "you think I would tell you?"
         }
     }
 ]
--- test/api/data/d_array_example.js
+++ test/api/data/d_array_example.js
@@ -1,12 +1,12 @@
 [
     {
-    surname: "Accepted",
-    forename: "John",
-    accepted: true,
-    _id: ObjectId("51a6182aea4ea77715000005"),
-    specialSubjects: [
+    "surname": "Accepted",
+    "forename": "John",
+    "accepted": true,
+    "_id": ObjectId("51a6182aea4ea77715000005"),
+    "specialSubjects": [
     "English"
     ],
-    __v: 0
+    "__v": 0
     }
 ]
