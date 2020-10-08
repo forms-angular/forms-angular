d7322c51083fba424bd502f74d71a958f687a2b2
Mark Chapman <mark.chapman@gmail.com>
2020-10-08 02:22:09 +0000

    Allow modelNameDisplay to be set by a promise

--- website/app/scripts/controllers/b-enhanced-schema-justnameandpostcode.js
+++ website/app/scripts/controllers/b-enhanced-schema-justnameandpostcode.js
@@ -1,11 +1,18 @@
 'use strict';
-websiteApp.controller('BEnhancedSchemaJustnameandpostcodeCtrl', ['$scope', function ($scope) {
+websiteApp.controller('BEnhancedSchemaJustnameandpostcodeCtrl', ['$scope', '$q', function ($scope, $q) {
 
   $scope.record = $scope.sharedData.record;
+
 
   $scope.sharedData.modelNameDisplay = 'Another override';
   $scope.sharedData.dropDownDisplay = 'Custom 2nd Level';
 
+  /* Alternatively you could do something async with $q type promise... */
+  $scope.sharedData.modelNameDisplayPromise = $q((resolve) => {
+    setTimeout(() => {
+      resolve('Promise Surprise!!');
+    }, 1000);
+  });
 
   $scope.contextMenu = [
     {
