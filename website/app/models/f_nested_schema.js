bfa55e60d2aad35b5303073a17dfc3c6d7060bcd
Mark Chapman <mark.chapman@gmail.com>
2021-02-04 23:58:27 +0000

    Make LinkCtrl work in directives in array subDocs.  Update deps.

--- website/app/models/f_nested_schema.js
+++ website/app/models/f_nested_schema.js
@@ -88,6 +88,18 @@ FSchema.statics.form = function (layout) {
                 }
             };
             break;
+        case 'link':
+            formSchema = {
+                surname: {},
+                forename: {},
+                exams: {
+                    schema: {
+                        subject: {}, examDate: {}, score: {}, result: {},
+                        grader: { link: { linkOnly: false, label: true } }
+                    }
+                }
+            };
+            break;
     }
     return formSchema;
 };
