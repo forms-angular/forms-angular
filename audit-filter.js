6251c54485dd5b665d8c40ce7068a9142ade591c
Mark Chapman <mark.chapman@gmail.com>
2025-12-20 20:57:29 +0000

    Ignore what look like error audit entries

--- audit-filter.js
+++ audit-filter.js
@@ -13,24 +13,26 @@ child.exec("npm audit --omit dev --json", (error, stdout, stderr) => {
     for (const module in concerns.vulnerabilities) {
         let vulns = concerns.vulnerabilities[module].via;
         let moduleMitigations = mitigations[module];
-        for (let i=0; i < vulns.length; i++) {
+        for (let i = 0; i < vulns.length; i++) {
             let key = vulns[i].url;
-            if (!moduleMitigations) {
-                console.log(`Need to look at new ${module} vuln: ${key} ${vulns[i].title}`);
-            } else {
-                let mitigation = moduleMitigations.find(o => {
-                    return !!o[key];
-                });
-                if (mitigation) {
-                    mitigation = mitigation[key];
-                    if (mitigation.nextReview) {
-                        const reviewDate = new Date(mitigation.nextReview);
-                        if (reviewDate < new Date()) {
-                            console.log(`Need to review at ${module} vuln: ${key} ${mitigation.reviewBy}`);
-                        }
-                    }
-                } else {
+            if (key) {
+                if (!moduleMitigations) {
                     console.log(`Need to look at new ${module} vuln: ${key} ${vulns[i].title}`);
+                } else {
+                    let mitigation = moduleMitigations.find(o => {
+                        return !!o[key];
+                    });
+                    if (mitigation) {
+                        mitigation = mitigation[key];
+                        if (mitigation.nextReview) {
+                            const reviewDate = new Date(mitigation.nextReview);
+                            if (reviewDate < new Date()) {
+                                console.log(`Need to review at ${module} vuln: ${key} ${mitigation.reviewBy}`);
+                            }
+                        }
+                    } else {
+                        console.log(`Need to look at new ${module} vuln: ${key} ${vulns[i].title}`);
+                    }
                 }
             }
         }
